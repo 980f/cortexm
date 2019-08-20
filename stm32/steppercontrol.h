00644820496902a76e9337aed7c58c20d8d976dd
980f <andy.980f@gmail.com>
2019-08-19 20:22:10 -0500

    wrong repo, saving before moving

--- stm32/steppercontrol.h
+++ stm32/steppercontrol.h
@@ -1,20 +1,13 @@
 #ifndef STEPPERCONTROL_H
 #define STEPPERCONTROL_H
 
-#include "hasSettings.h"
+
 #include "cheapTricks.h"
-#include "markreport.h"
-struct GasPedal :public Settable{
+struct GasPedal {
   double start;
   double cruise;
   double accel;
   GasPedal(void);
-
-  int numParams()const{
-    return 3;
-  }
-  bool setParams(ArgSet&args);
-  void getParams(ArgSet&args)const;
 
   /**number of steps to achieve full speed*/
   double acceleration(void);
@@ -23,38 +16,8 @@ struct GasPedal :public Settable{
   }
 };
 
-/**
-  * wheel geometry is resendable with each homing request.
-  * between + across should be at least as big as the separation between stopping points,
-  * and can be bigger since both should be the largest of their kind which aren't typically adjacent.
-  */
-struct Homer:public Settable {
-  int unused; //
-  int widest; //width of biggest index mark
-  int offset; //per tray accumulated error in mark vs. sample window
 
-  Homer():
-    unused(0),
-    widest(0),
-    offset(0) {
-  }
-
-  int numParams()const{
-    return 3;
-  }
-  bool setParams(ArgSet&args);
-  void getParams(ArgSet&args)const;
-
-  bool notConfigured(){
-    return widest==0 ;
-  }
-  void setOffset(int off){
-    set(offset,off);
-  }
-
-};
-
-struct StepAccess:public Settable {
+struct StepAccess {
   int position; //absolute position in steps, negative allowed for circularity and home offset.
   double hz;
 
@@ -62,9 +25,6 @@ struct StepAccess:public Settable {
   bool clockwise;
   u32 ticks;
 
-  int numParams()const;
-  bool setParams(ArgSet&args);
-  void getParams(ArgSet&args)const;
 
   void setPosition(int pos){
     set(position,pos);
@@ -72,31 +32,24 @@ struct StepAccess:public Settable {
 };
 
 
-class StepperControl : public HasSettings {
+class StepperControl {
 public:
   StepAccess a;
-  Homer h;
   GasPedal v;
   StepperControl(void);
-private:
 public:
-  bool setParam(ID fieldID, ArgSet&args);
-  void touch(ID fieldID);
-  bool getParam(ID fieldID, ArgSet&args)const;
   /** trigger a recentering even if c.position and h stuff doesn't change.*/
   void retrigger(bool h2);
   void setTarget(int step);
-  /** @return whether a message has been made pending */
-  bool propagate(StepperControl &other,bool diagsToo);
+
 };
 
-struct MotorReport:public Settable {
+struct MotorReport {
   int motionCode;
   int homingStage;
   bool hasHomed;
   int location;
   int target;
-  MarkReport markReport;
 
   MotorReport():
     motionCode(),
@@ -107,11 +60,6 @@ struct MotorReport:public Settable {
     markReport() {
   }
 
-  int numParams()const {
-    return 5+markReport.numParams();
-  }
-  bool setParams(ArgSet&args);
-  void getParams(ArgSet&args)const;
 };
 
 
