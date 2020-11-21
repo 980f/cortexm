042df399752cca541a22e544f2de4ae582e9665d
andyh <andy.980f@gmail.com>
2020-11-21 10:23:44 -0600

    auto config of io pins

---
+++ stm32/pinconfigurator.cpp
@@ -0,0 +1,5 @@
+//
+// Created by andyh on 11/20/20.
+//
+
+#include "pinconfigurator.h"
---
+++ stm32/pinconfigurator.h
@@ -0,0 +1,11 @@
+//
+// Created by andyh on 11/20/20.
+//
+
+#ifndef L452EVAL_PINCONFIGURATOR_H
+#define L452EVAL_PINCONFIGURATOR_H
+
+class pinconfigurator {
+};
+
+#endif //L452EVAL_PINCONFIGURATOR_H
