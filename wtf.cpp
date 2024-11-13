5bf6907b704a83ca8fb6d5f3f59ca085cdf042a5
andyh <andy.980f@gmail.com>
2024-11-13 10:53:00 -0600

    allow for hardware data value breakpoint. Untested.

--- wtf.cpp
+++ wtf.cpp
@@ -1,18 +1,16 @@
 #include "wtf.h"
 #include "cheaptricks.h"
 
-extern "C" bool wtf(int complaint) {
+extern "C" bool wtf(const int complaint) {
   static int lastWtf = 0;
   static int repeats = 0;
-  if (complaint) {
-    if (changed(lastWtf, complaint)) {
+  if (int databreakpoint=complaint) {
+    if (changed(lastWtf, databreakpoint)) {
       repeats = 0;
       return false;
-    } else {
-      ++repeats;
-      return true;
     }
-  } else {
-    return false;
+    ++repeats;
+    return true;
   }
+  return false;//wtf(0) does nothing at all
 }
