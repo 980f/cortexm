25cc26df9aaae7d8d77ef35bd68328da30b2f97e
andyh <andy.980f@gmail.com>
2025-02-01 21:30:49 -0600

    clocks do not get the same managment as dma and flash, there is a common header and then each chip implements that specification

--- stm32/clocks.h
+++ stm32/clocks.h
@@ -1,8 +1,28 @@
+#pragma once
 
-#if DEVICE==103
-#include "clocksf1.h"
-#elif DEVICE==407 || DEVICE==411
-#include "clocksf4.h"
-#elif DEVICE==452
-#include "clocksl4.h"
-#endif
+#include "eztypes.h"
+
+/** processor oscillator setp and support */
+
+extern const u32 EXTERNAL_HERTZ;
+
+//clock rate:
+/** bus is: 0:ahb; 1:apb1; 2:apb2; 3:adc; -1:sysclock;*/
+u32 clockRate(unsigned int bus);
+
+/**set system clocks to the fastest possible*/
+void warp9(bool internal);
+/**MCO pin configuration (to snoop on internal clock)*/
+void setMCO(unsigned int mode);
+
+/** this class exists to run clock setup code at a user selectable init level.
+ Usage:
+ClockStarter InitStep(InitHardware-100) (false,0,1000);
+*/
+struct ClockStarter {
+  const bool intosc;//hs oscillator selection
+  const u32 coreHertz;
+  const u32 sysHertz;
+  ClockStarter(bool intosc,u32 coreHertz,u32 sysHertz);
+};
+
