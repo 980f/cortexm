285db1b6a2d0d3c79653a0e32cb6962c9650e3c3
andyh <andy.980f@gmail.com>
2025-02-11 11:11:47 -0600

    appeased constexpr on type casting, but still struggling with IDE's prettyprinting being inconsistent

--- bitbandcalculator.cpp
+++ bitbandcalculator.cpp
@@ -11,28 +11,31 @@ mv a.out whateveryouwishtocallthis
 #include "stdlib.h"
 
 #include "bitbanding.h"
+using namespace BandAid;
 
 unsigned asHex(const char *heximage) {
   return strtoul(heximage, nullptr, 16);
 }
 
-const BandAid bah(0x40010000,17);
-const BandAid hum(0x42048030);
+
+const Banded bah(0x40010000,17);
+const Banded hum(0x42048030);
 
 int main(int argc, char *argv[]) {
   switch (--argc) {
     case 2: //address  in hexadecimal and bit, print  band address
-      printf("BB: %08x \t from:%s.%s", unsigned(BandAid(asHex(argv[1]), atoi(argv[2]))), argv[1], argv[2]);
+      printf("BB: %08x \t from:%s.%s", unsigned(Banded(asHex(argv[1]), atoi(argv[2]))), argv[1], argv[2]);
       break;
-    case 1: //bitband to address and bit
-    { BandAid aid(asHex(argv[1]));
-      printf("WA: %08x.%u \t from:%s", aid.byteAddress(), aid.bit(), argv[1]);
+    case 1:{ //bitband to address and bit
+      Banded aid(asHex(argv[1]));
+      unsigned wordAddress=Extractor(aid);
+      printf("WA: %08x.%u \t from:%s", wordAddress, aid.bitnum, argv[1]);
     }
       break;
     case 0:
       printf("usage:\n one arg is bitband address, two is byte address in hex and bit number:");
-      printf("\n %08x %d -> %08x", bah.extractor.pattern,bah.parser.b.bitnum, unsigned(bah));
-      printf("\n %08x -> %08x %u", hum.parser.pattern, hum.byteAddress(), hum.bit());
+     printf("\n %08x %d -> %08x", unsigned(Extractor(bah)),bah.bitnum, unsigned(bah));
+     printf("\n %08x -> %08x %u", unsigned(hum), unsigned(Extractor(hum)), hum.bitnum);
       break;
   }
   printf("\n");
--- bitbanding.h
+++ bitbanding.h
@@ -11,57 +11,68 @@
  */
 
 #include "bitbasher.h"
+#include <cstdint>
 
 namespace BandAid {
-
+  struct Banded;//need to predeclare as each class refers to the other
   struct Extractor {
     unsigned byteNumber: 2;
     unsigned offset: 18;
     unsigned alwaysZeroes: 9;
     unsigned space: 3;
-    constexpr Extractor(unsigned pattern){
-      byteNumber=extractField(pattern, 1, 0);
-      offset=extractField(pattern,19,2);
-      alwaysZeroes=extractField(pattern,28,20);
-      space=extractField(pattern, 31, 29);
+
+    //type punning via offical mechanisms was not compatible with constexpr rules as of c++17, would have worked with c++23.
+    constexpr Extractor(uint32_t pattern):
+      byteNumber(extractField(pattern, 1, 0)),
+      offset(extractField(pattern,19,2)),
+      alwaysZeroes(extractField(pattern,28,20)),
+      space(extractField(pattern, 31, 29)) {}
+
+    constexpr operator uint32_t () const {
+      return Punner<uint32_t,Extractor>(*this).desired;//HAH! This doesn't work on PC architecture
     }
-    struct Banded;
-    constexpr Extractor(const Banded &bandAddress);  
+
+    constexpr Extractor(const Banded &bandAddress);
   };
 
   struct Banded {
+
     unsigned unused: 2; //always zero, should be ignored by hardware but don't challenge that.
     unsigned bitnum: 5; //32 bits
     unsigned offset: 18; //2^20 bytes is 2^18 words
-    unsigned indicated: 1; //should always be a 010 if this is a valid band address
-    unsigned alwaysZeroes: 3;
-    unsigned space: 3; 
+    unsigned indicated : 1; //should always be a 1 if this is a valid band address
+    unsigned alwaysZeroes : 3; //should always be zero if this is a valid band address
+    unsigned space : 3;
 
-    constexpr Banded(unsigned address,unsigned bitNumber){
-      Extractor e(address);
-      unused=0;
-      bitnum=e.byteNumber*8+bitNumber;
-      offset=e.offset;
-      indicated=1;
-      alwaysZeroes=0;
-      space=e.space; 
+    constexpr operator uint32_t() const {
+      return bitnum << 2 | offset << 7 | indicated << 25 | space << 29;
     }
 
-    constexpr Banded(unsigned bandaddress){
-      unused=0;//4debug
-      bitnum=extractField(bandaddress,6, 2);
-      offset=extractField(bandaddress, 24,7); 
-      indicated=bitFrom(bandaddress,25);// if not a 1 then this was not a band address
-      alwaysZeroes=extractField(bandaddress,28,26);
-      space=extractField(bandaddress,31,29);
-    };
-  };
+    constexpr Banded(Extractor e,unsigned bitNumber):unused(0),bitnum(bitNumber | (e.byteNumber * 8)),offset(e.offset),indicated(1),alwaysZeroes(0),space(e.space){}
 
-  constexpr unsigned bandFor(unsigned address,unsigned bitNumber){
-    Extractor e(address);
-    bitNumber+= e.byteNumber<<3; //in case someone gives byte and bit within byte instead of word and bit within word. Other combos are GIGO.    
-    return e.space<<29 | 1<<25 | e.offset<<7 | bitNumber <<2;
+    constexpr Banded(uint32_t address,unsigned bitNumber):Banded(Extractor(address),bitNumber){}
+
+    //type punning via offical mechanisms was not compatible with constexpr rules as of c++17, would have worked with c++23.
+    constexpr Banded(uint32_t bandaddress=0) : unused(0)
+        , bitnum(extractField(bandaddress, 6, 2))
+        , offset(extractField(bandaddress, 24, 7))
+        , indicated(bitFrom(bandaddress, 25))
+        , alwaysZeroes(extractField(bandaddress, 28, 26))
+        , space(extractField(bandaddress, 31, 29))
+    {}
+
+    constexpr Banded(Banded &&other) = default;
+    constexpr Banded(const Banded &other) = default;
+
+  } ;
+
+  constexpr Extractor::Extractor(const Banded &bandAddress):byteNumber(bandAddress.bitnum/8),offset(bandAddress.offset),alwaysZeroes(0),space(bandAddress.space){}
+
+  /** bit band uses the word address as seen by the bus as a byte+bitnumber. To do that the bitnumber has to be placed somewhere in the word address, which is the bitNumber<<2 clause.
+  * The word address needs to move out of the way of the bitnumber's shift, and the bitnumber is 5 bits so we get the offset<<7, 7+2+5 term.
+  * Bit 25 is apparently an indicator to the bus interface that this is a bitband request and it needs to futz with the address, and the top 3 bits are the cortex M data typing selection. */
+  constexpr uint32_t bandFor(uint32_t address,unsigned bitNumber){
+    return Banded(address,bitNumber);// e.space<<29 | 1<<25 | e.offset<<7 | bitNumber <<2;
   }
 
 }
-
